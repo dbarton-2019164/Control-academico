3425c850c4510372e94989998bebd47d7dbc25b7
jgalicia <jgalicia-2022203@kinal.edu.gt>
2024-02-12 08:39:52 -0600

    Estructura inicial del proyecto

---
+++ models/server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const cors = require("cors");
+const { dbConnection } = require("../db/config");
+
+class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
+    this.usuarioPath = "/academic/usuarios";
+
+    this.conectarDB();
+
+    this.middlewares();
+
+    this.routes();
+  }
+
+  async conectarDB() {
+    await dbConnection();
+  }
+
+  middlewares() {
+    this.app.use(express.static("public"));
+    this.app.use(cors());
+    this.app.use(express.json());
+  }
+
+  routes() {
+    this.app.use(this.usuarioPath, require("../routes/user.routes"));
+  }
+
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log(
+        `Servidor ejecutando y escuchando por el puerto ${this.port}`
+      );
+    });
+  }
+}
+
+module.exports = Server;
